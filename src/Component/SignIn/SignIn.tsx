5f8b45c8b0dcb5c59c48a83a273ba27cb4d4fbb6
D-D-f <defreitas-david@outlook.fr>
2024-02-10 11:51:51 +0100

    add feature remember

--- src/Component/SignIn/SignIn.tsx
+++ src/Component/SignIn/SignIn.tsx
@@ -1,24 +1,38 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 import { useForm } from "react-hook-form";
-import {authentication} from "../../Requete/authentication.ts";
+import { authentication } from "../../Requete/authentication.ts";
 import "../../assets/scss/PagesStyle/SignIn.scss";
-import {useState} from "react";
+import { useState, useEffect } from "react";
 
 const SignIn = () => {
-    const [remember, setRemember] = useState(false)
-    const {register, handleSubmit} = useForm();
+    const [remember, setRemember] = useState(false);
+    const { register, handleSubmit, setValue, getValues } = useForm();
 
-    const onSubmit = async (d) => {
-        const connectionUser = await authentication(d);
+    useEffect(() => {
+        if (localStorage.getItem('remember')) {
+            const user = JSON.parse(localStorage.getItem('remember'));
+            setValue('email', user.email);
+            setValue('password', user.password);
+            setRemember(true);
+        }
+    }, [setValue]);
 
-        if(connectionUser.status === 200 && remember && !localStorage.getItem('remember')) {
-            localStorage.setItem("remember", JSON.stringify(d));
+    const onSubmit = async (data) => {
+        const connectionUser = await authentication(data);
+
+        if (connectionUser.status === 200 && remember && !localStorage.getItem('remember')) {
+            localStorage.setItem("remember", JSON.stringify(data));
         }
     }
 
     const activeRemember = (e) => {
-        setRemember(e.target.checked)
+        setRemember(e.target.checked);
+
+        if (!e.target.checked) {
+            // Effacer les valeurs du localStorage si "Remember me" est décoché
+            localStorage.removeItem('remember');
+        }
     }
 
     return (
@@ -28,14 +42,14 @@ const SignIn = () => {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="InputSignIn">
                     <label htmlFor="email">Username</label>
-                    <input type="text" id="email" {...register('email')} />
+                    <input type="text" id="email" {...register('email', { required: true })} defaultValue={remember ? getValues('email') : ''} />
                 </div>
                 <div className="InputSignIn">
                     <label htmlFor="password">Password</label>
-                    <input type="password" id="Password" {...register('password')} />
+                    <input type="password" id="Password" {...register('password', { required: true })} defaultValue={remember ? getValues('password') : ''} />
                 </div>
                 <div className="input-remember">
-                    <input type="checkbox" id="remember-me" name="remember-me" onChange={activeRemember}/>
+                    <input type="checkbox" id="remember-me" name="remember-me" checked={remember} onChange={activeRemember} />
                     <label htmlFor="remember-me">Remember me</label>
                 </div>
 
