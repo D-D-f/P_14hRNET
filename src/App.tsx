bf3b8acf1acdfa94b87d68c130f4257906e8ef77
D-D-f <defreitas-david@outlook.fr>
2024-02-11 17:59:23 +0100

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import Header from "./Component/Header/Header.tsx";
-import UserPage from "./Pages/UserPage/UserPage.tsx";
-import Footer from "./Component/Footer/Footer.tsx";
-import "./assets/scss/PagesStyle/App.scss";
-
 const App = () => {
     return (
         <div className="App">
-            <Header />
-            <main>
-                <UserPage />
-            </main>
-            <Footer />
+
         </div>
     )
 }
